d1a04624047534077db570d6e3c0c25d9da348ad
Jamie <jamieaspainhower@gmail.com>
2025-01-25 18:14:20 -0800

    init unlockmouse

--- Unlock Mouse Toggle/UnlockMouse.cs
+++ Unlock Mouse Toggle/UnlockMouse.cs
@@ -1,1 +1,29 @@
-using System;
+using Vintagestory.API.Common;
+using WhatInTheWorld.MadeUpGarabage.Nope;
+
+namespace Unlock_Mouse_Toggle;
+
+public class Unlock_Mouse_Toggle : ModSystem
+{
+    // Called on server and client
+    // Useful for registering block/entity classes on both sides
+
+    #region unneeded
+    /*
+    public override void Start(ICoreAPI api)
+    {
+        api.Logger.Notification("Hello from template mod: " + api.Side);
+    }
+
+    public override void StartServerSide(ICoreServerAPI api)
+    {
+        api.Logger.Notification("Hello from template mod server side: " + Lang.Get("mouselocktoggle:hello"));
+    }
+    */
+    #endregion
+
+    public override void StartClientSide(ICoreClientAPI api)
+    {
+        api.Logger.Notification("Hello from test mod client side: " + Lang.Get("mouselocktoggle:hello"));
+    }
+}
